f7a3c1e4653982211fe2aad6567e4f1c135065f6
onusteam <jgarces@onus.cl>
2014-11-10 11:46:02 -0300

    Agregada la clase Operaciones donde se aplica los metodos de sumar y restar.

---
+++ App/Operaciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Operaciones
+    {
+        public int Sumar (int valor1, int valor2)
+        {
+            int resultado = valor1 + valor2;
+
+            return resultado;
+        }
+
+        public int Restar(int valor1, int valor2)
+        {
+            return valor1 + valor2;        
+        }
+
+
+
+    }
+}
