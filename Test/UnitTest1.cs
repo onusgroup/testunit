e75aba8ea996c67d2658e345327ad54ecf93ea86
onusteam <jgarces@onus.cl>
2014-11-10 13:07:31 -0300

    Agregada las explicaciones de cada miembro que expone la clase Assert.

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -10,13 +10,19 @@ namespace Test
         [TestMethod]
         public void SumarTest()
         {
-            var Expected = new int();
-            var Expected2 = 8;
+
+            var Expected = 8;
             Operaciones op = new Operaciones();
             var resultado = op.Sumar(2, 6);
 
-            Assert.ReferenceEquals(Expected, resultado);
-            Assert.AreEqual(Expected2, resultado);
+
+            //TODO: Assert comprueba las condiciones en pruebas unitarias utilizando proposiciones verdaderas o falsas.
+
+            //TODO: Comprueba si dos objetos especificados son iguales.
+            Assert.AreEqual(Expected, resultado);
+            //TODO: Comprueba si el objeto especificado no es null. Se produce un error en la aserción si es null.
+            Assert.IsNotNull(Expected);
+
 
         }
     }
