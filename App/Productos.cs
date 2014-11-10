4218392b5117d8dab4d251ec148eaa8a0e7a401b
onusteam <jgarces@onus.cl>
2014-11-10 15:28:03 -0300

    Agregadas las clases Productos y Ventas

---
+++ App/Productos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public static class Productos
+    {
+        public class Producto
+        {
+            public int id { get; set; }
+            public string grupo { get; set; }
+            public string album { get; set; }
+            public string genero { get; set; }
+            public string formato { get; set; }
+            public int stock { get; set; }
+        }
+       
+
+        public static List<Producto> getListaProductos()
+        {
+            List<Producto> lista = new List<Producto>() 
+            { 
+                new Producto(){ id = 1, grupo = "Metallica", album = "Kill Em All", genero = "Trash Metal", formato="MP3", stock=200},
+                new Producto(){ id = 2, grupo = "Ac/dc", album = "Back in Black", genero = "Rock", formato="Bluray", stock=200},
+                new Producto(){ id = 3, grupo = "Megadeth", album = "Rust in Peace", genero = "Trash Metal", formato="DVD", stock=200},
+                new Producto(){ id = 4, grupo = "Metallica", album = "Master Of Puppets", genero = "Trash Metal", formato="Vinilo", stock=200},
+                new Producto(){ id = 5, grupo = "Los Prisioneros", album = "Pateando Piedras", genero = "Rock", formato="MP3", stock=200}
+            };
+            return lista;
+        }
+
+
+    }
+
+}
---
+++ App/Ventas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public static class Ventas
+    {
+        public class Venta
+        {
+            public int id { get; set; }
+            public string cliente { get; set; }
+            public string fecha { get; set; }
+            public int cantidad { get; set; }
+        }
+
+        public static List<Venta> getListaVentas()
+        {
+            List<Venta> lista = new List<Venta>() 
+            { 
+                new Venta(){ id=1, cliente="raul", cantidad=20, fecha="12/05/2013"},
+                new Venta(){ id=2, cliente="andres", cantidad=12, fecha="05/11/2013"},
+                new Venta(){ id=3, cliente="pato", cantidad=50, fecha="22/02/2013"},
+            };
+
+            return lista;
+        }
+
+
+    }
+}
