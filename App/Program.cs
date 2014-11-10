2ef5e1744413c8516d4c8d0aaf86c790b91125da
onusteam <jgarces@onus.cl>
2014-11-10 11:53:00 -0300

    Agregada las llamadas para probar el metodo Operaciones

--- App/Program.cs
+++ App/Program.cs
@@ -10,6 +10,13 @@ namespace App
     {
         static void Main(string[] args)
         {
+            Operaciones op = new Operaciones();
+            int res = op.Sumar(10, 20);
+            int res2 = op.Restar(100, 30);
+
+            Console.WriteLine("Suma: {0} - Resta: {1}",res, res2);
+            Console.ReadKey();
+
         }
     }
 }
